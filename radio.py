9e58b6e652d14d0d9017ae388ed540c27022124f
JohanAndreyGuarinReatiga <186079123+JohanAndreyGuarinReatiga@users.noreply.github.com>
2024-10-29 13:08:38 -0500

    Ejercicio de saludo y radio ya completados, hacen falta el resto

---
+++ radio.py
@@ -0,0 +1,10 @@
+#Escriba un programa que reciba como entrada el radio de un círculo y entregue como salida su perímetro y su área:
+
+#Ingrese el radio: 5
+#Perimetro: 31.4
+#Área: 78.5
+import math
+radius = int(input("Enter the radius of a circle "))
+perimeter = 2 * math.pi * radius
+area = math.pi * radius**2
+print(f""" The perimeter of the circle is: {perimeter} and the area is {area}""")
