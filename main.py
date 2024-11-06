6ae804d5d64332637d2f592d7e8f803198d1da6c
JohanAndreyGuarinReatiga <186079123+JohanAndreyGuarinReatiga@users.noreply.github.com>
2024-11-06 11:39:19 -0500

    Solucion ejercicio parte decimal

--- main.py
+++ main.py
@@ -1,19 +1,8 @@
 
-#Hora futura
-#Escriba un programa que pregunte al usuario la hora actual t del reloj y un número entero de horas h, que indique qué hora marcará el reloj dentro de h horas:
+number = float(input("Enter a number: "))
+DecimalPart = (number - int(number))
 
-#Hora actual: 3
-#Cantidad de horas: 5
-#En 5 horas, el reloj marcara las 8
-#Hora actual: 11
-#Cantidad de horas: 43 
-#En 43 horas, el reloj marcara las 6
-
-t= int(input("Enter the actual time: "))
-h = int(input("Enter the hours: "))
-
-FutureTime = (t + h) % 24
-
-print(f"""
-      "In {h} hours, the clock will mark {FutureTime}"
+print(f"""  
+ "Decimal part: {DecimalPart}"
 """)
+
